bbd3b2fd02e4db4a9f2ea502d37bbca8e1ebf932
brayancadavid <bcadavid@unicesar.edu.co>
2024-11-18 22:33:22 -0500

    @fixed url notifications, changed by route of gateway

--- src/main/java/com/hexagon/events_service/service/NotificationService.java
+++ src/main/java/com/hexagon/events_service/service/NotificationService.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class NotificationService {
 
-    private static final String NOTIFICATION_SERVICE_URL = "https://notifications-service-fq4c.onrender.com/api/notifications";
+    private static final String NOTIFICATION_SERVICE_URL = "https://cloud-gateway-sg8j.onrender.com/api/notifications";
     private final RestTemplate restTemplate;
     private RabbitMQJsonProducer jsonProducer;
 

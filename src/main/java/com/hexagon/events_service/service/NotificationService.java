756af60e9e620bfad116c4541003820c8d7be7ff
brayancadavid <bcadavid@unicesar.edu.co>
2024-11-18 23:05:16 -0500

    @fix:testing solution

--- src/main/java/com/hexagon/events_service/service/NotificationService.java
+++ src/main/java/com/hexagon/events_service/service/NotificationService.java
@@ -2,8 +2,6 @@ package com.hexagon.events_service.service.notification;
 
 import com.hexagon.events_service.publisher.RabbitMQJsonProducer;
 import com.hexagon.events_service.dto.NotificationDTO;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.appinfo.InstanceInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,22 +9,19 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class NotificationService {
 
+    private static final String NOTIFICATION_SERVICE_URL = "https://NOTIFICATIONS/api/notifications";
     private final RestTemplate restTemplate;
-    private final RabbitMQJsonProducer jsonProducer;
-    private final EurekaClient eurekaClient;
+    private RabbitMQJsonProducer jsonProducer;
 
     @Autowired
-    public NotificationService(RestTemplate restTemplate, RabbitMQJsonProducer jsonProducer, EurekaClient eurekaClient) {
+    public NotificationService(RestTemplate restTemplate,RabbitMQJsonProducer jsonProducer) {
         this.restTemplate = restTemplate;
         this.jsonProducer = jsonProducer;
-        this.eurekaClient = eurekaClient;
     }
 
+    
     public void notify(NotificationDTO notificationDTO) {
-        InstanceInfo instance = eurekaClient.getNextServerFromEureka("NOTIFICATIONS", false);
-        String notificationServiceUrl = instance.getHomePageUrl() + "/api/notifications";
-
-        restTemplate.postForObject(notificationServiceUrl, notificationDTO, NotificationDTO.class);
+        restTemplate.postForObject(NOTIFICATION_SERVICE_URL, notificationDTO, NotificationDTO.class);
         jsonProducer.sendJsonNotification(notificationDTO);
     }
 }
